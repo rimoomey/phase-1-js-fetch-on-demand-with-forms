49f4248412e1aabf516b05c561b9cf61bcc044f9
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-04 10:33:33 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+  const submitSearch = document.querySelector("#search-form");
+
+  submitSearch.addEventListener("submit", searchEvent);
+};
+
+function searchEvent(event) {
+  event.preventDefault();
+  let ID = event.target.children[1].value;
+
+  fetch("http://localhost:3000/movies")
+    .then((response) => response.json())
+    .then((data) => {
+      const target = data.find((data) => data.id === parseInt(ID));
+      displayBook(target);
+    });
 }
 
-document.addEventListener('DOMContentLoaded', init);
+function displayBook(book) {
+    const displayArea = document.querySelector('#movie-details')
+
+    if(book != undefined) {
+        displayArea.children[0].textContent = `Title: ${book.title}`;
+        displayArea.children[1].innerHTML = "Summary: <br><br>" + book.summary;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", init);
